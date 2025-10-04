4749731966ffb73b8bb0f75eeca710f52b41445f
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-10-04 15:42:56 +0200

    feat: add endpoint for getting certain flag

--- src/routes/flagRoutes.ts
+++ src/routes/flagRoutes.ts
@@ -8,6 +8,7 @@ export async function flagRoutes(fastify: FastifyInstance): Promise<void> {
 
 	fastify.post('/flags', flagController.createFlag.bind(flagController));
 	fastify.get('/flags', flagController.getAllFlags.bind(flagController));
+	fastify.get('/flags/:id', flagController.getFlagById.bind(flagController));
 	fastify.put('/flags/:id', flagController.updateFlag.bind(flagController));
 	fastify.patch('/flags/:id/toggle', flagController.toggleFlag.bind(flagController));
 	fastify.post('/flags/:id/rules', flagController.addRuleToFlag.bind(flagController));
